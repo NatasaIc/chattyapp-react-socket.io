56175cee2f1ae9e60dafe69420125d39f7e2fb8f
ZoeMesaros <zoe.mesaros@hotmail.com>
2023-08-16 09:16:04 +0200

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
 function App() {
-  return <div>App</div>;
+  const [username, setUsername] = useState("");
+
+  const socket = io("http://localhost:3000", { autoConnect: false });
+
+  useEffect(() => {
+    socket.on("new_user_connected", (username) => {
+      console.log(username);
+    });
+  }, []);
+
+  const initChat = () => {
+    socket.connect();
+
+    socket.emit("user_connected", username);
+  };
+
+  return (
+    <div className="App">
+      <input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        type="text"
+        placeholder="Välj ett användarnamn..."
+      />
+      <button onClick={initChat}>Börja chatta</button>
+      <p>{username}</p>
+    </div>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -2,20 +2,27 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const app = express();
 
+const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors:{
-        origin: "*",
-    },
+  cors: {
+    origin: "*",
+  },
 });
 
+app.use(cors());
+
+app.use(express.static("public"));
+
 io.on("connection", (socket) => {
-    console.log("Newuser connected: ", socket.id);
+  console.log("New user connected: ", socket.id);
+
+  socket.on("user_connected", (username) => {
+    socket.broadcast.emit("new_user_connected", username);
+  });
 });
 
 console.log("hej");
 
 server.listen(3000, () => console.log("Servern körs på localhost: 3000!"));
-
